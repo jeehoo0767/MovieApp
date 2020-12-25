5f2198c543d08ed5aec38de57a98d78b5ceb58b9
jeehoo0767 <jeehoo0767@naver.com>
2020-12-26 08:13:13 +0900

    MainPage Template

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,44 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { FaCode } from "react-icons/fa";
+import MainImage from './Sections/MainImage'
+import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config'
 
 function LandingPage() {
+    
+    const [Movies, setMovies] = useState([]);
+    const [MainMovieImage, setMainMovieImage] = useState(null)
+
+    useEffect(() => {
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+
+        fetch(endpoint)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response)
+            setMovies([response.results])
+            setMainMovieImage(response.results[0])
+        })
+    }, [])
+
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-            </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
+        <div style={{ width : '100%', margin : '0'}}>
+            {MainMovieImage && 
+                <MainImage 
+                    image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+                    title = {MainMovieImage.original_title}
+                    description = {MainMovieImage.overview}
+                /> 
+            }
+           <div style={{ width:'85%', margin : '1rem auto'}}>
+                <h2>Movies by latest</h2>
+                <hr/>
+
+           </div>
+
+           <div style={{display : 'flex', justifyContent : 'center'}}>
+                <button>Load More</button>
+           </div>
+        </div>
     )
 }
 

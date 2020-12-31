83262e2ef6677444b7b683343b36a58460e1639f
jeehoo0767 <jeehoo0767@naver.com>
2020-12-31 11:08:44 +0900

    Actor GridCard Component

--- client/src/components/views/MovieDetail/MovieDetail.js
+++ client/src/components/views/MovieDetail/MovieDetail.js
@@ -1,13 +1,16 @@
 import React, {useState, useEffect} from 'react'
+import {Row} from 'antd'
 import {API_KEY, API_URL, IMAGE_BASE_URL} from '../../Config'
 import MainImage from '../LandingPage/Sections/MainImage'
 import MovieInfo from './Section/MovieInfo'
+import GridCard from '../commons/GridCard'
 
 function MovieDetail(props) {
 
     let movieId = props.match.params.movieId
 
     const [Movie, setMovie] = useState([])
+    const [Casts, setCasts] = useState([])
 
     useEffect(() => {
 
@@ -20,6 +23,13 @@ function MovieDetail(props) {
        .then(response => {
            console.log(response)
            setMovie(response)
+       })
+
+       fetch(endpointCrew)
+       .then(response => response.json())
+       .then(response => {
+           console.log(response)
+           setCasts(response.cast)
        })
     }, [])
 
@@ -42,6 +52,17 @@ function MovieDetail(props) {
                 <div style={{ display:'flex', justifyContent : 'center', margin : '2rem'}}>
                     <button>Toggle Actor View</button>
                 </div>
+                <Row gutter={[16, 16]}>
+                    {Casts && Casts.map((cast, index) => {
+                        return <React.Fragment key={index}>
+                            <GridCard 
+                                image={cast.profile_path? `${IMAGE_BASE_URL}w500${cast.profile_path}` : null}
+                                movieId={cast.id}
+                                movieName={cast.original_title}
+                            />
+                        </React.Fragment>
+                    })}
+                </Row>
             </div>
         </div>
     )

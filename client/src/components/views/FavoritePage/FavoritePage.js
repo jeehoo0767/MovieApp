01a0e5059d29a35d1151e173d2a5548c1da7f074
jeehoo0767 <jeehoo0767@naver.com>
2021-01-03 18:50:21 +0900

    Favorite Page Template

---
+++ client/src/components/views/FavoritePage/FavoritePage.js
@@ -0,0 +1,41 @@
+import React, {useEffect} from 'react'
+import './favorite.css'
+import Axios from 'axios'
+
+function FavoritePage() {
+
+    useEffect(() => {
+        Axios.post('/api/favorite/getFavoredMovie', { userFrom : localStorage.getItem('userId')})
+        .then(response => {
+            if(response.data.success) {
+
+            } else {
+                alert('영화 정보 가져오기 실패')
+            }
+        })
+    }, [])
+
+    return (
+        <div>
+            <div style={{ width : '85%', margin : '3rem auto'}}>
+                <h3>Favorite Movies</h3>
+                <hr/>
+
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Movie Title</th>
+                            <th>Movie Runtime</th>
+                            <th>Remove from favorites</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
+
+export default FavoritePage
--- client/src/components/views/NavBar/Sections/LeftMenu.js
+++ client/src/components/views/NavBar/Sections/LeftMenu.js
@@ -19,6 +19,9 @@ function LeftMenu(props) {
         <Menu.Item key="setting:4">Option 4</Menu.Item>
       </MenuItemGroup>
     </SubMenu>
+    <Menu.Item key="Favorite">
+      <a href="/favorite">Favorite</a>
+    </Menu.Item>
   </Menu>
   )
 }

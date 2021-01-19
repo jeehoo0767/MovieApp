17ba7611caf5369fc043e7730b57d7047ecf38b4
jeehoo0767 <jeehoo0767@naver.com>
2021-01-19 17:44:43 +0900

    Search Component Template

--- client/src/components/views/SearchMovies/SearchMovies.js
+++ client/src/components/views/SearchMovies/SearchMovies.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useDispatch } from "react-redux";
+import {Space, Input} from 'antd';
+import {changeValue} from '../../../_actions/value_action'
 
 function SearchMovies() {
+    
+    const [SearchValue, setSearchValue] = useState("")
+
+    const { Search } = Input;
+
+    const dispatch = useDispatch()
+
+    const onSearchValueChange = (e) => {
+        setSearchValue(e.target.value)
+        dispatch(changeValue(e.target.value))
+      }
+
     return (
-        <div>
-            SearchMovies
+        <div style={{ width : '100%', padding : '20px', display : 'flex', justifyContent : 'center'}}>
+            <Space style= {{ width : '30%'}} direction="vertical">
+                <Search placeholder="input search text" value ={SearchValue} onChange = {onSearchValueChange} onSearch enterButton style={{marginTop : '8px'}}/>
+            </Space>
         </div>
     )
 }
